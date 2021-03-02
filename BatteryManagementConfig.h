5d436969038fa0e26e4d91d81d9db2288fa3b103
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-03-02 20:43:24 +0530

    Adding Low  and High alert message
    Adding Low  and High alert message

--- BatteryManagementConfig.h
+++ BatteryManagementConfig.h
@@ -2,13 +2,14 @@ typedef struct
 {
 	float min_range;
 	float max_range;
-	char Alartmessage[50];
+	char Min_Alertmessage[50];
+	char Max_Alertmessage[50];
 }BatteryManageConfig;
 
 
-BatteryManageConfig temperature_config={0,45,"Temperature out of range!"};
-BatteryManageConfig soc_config={20,80,"State of Charge out of range!"};
-BatteryManageConfig chargeRate_config={0,0.8,"Charge Rate out of range!"};
+BatteryManageConfig temperature_config={0,45,"Temperature is very low!","Temperature is very High!"};
+BatteryManageConfig soc_config={20,80,"State of Charge is very low!","State of Charge is very High!"};
+BatteryManageConfig chargeRate_config={0,0.8,"Charge Rate is very low!","Charge Rate is very low!"};
 
 
 enum BatteryHealth{
