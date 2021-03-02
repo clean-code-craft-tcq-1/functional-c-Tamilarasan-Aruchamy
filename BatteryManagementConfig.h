4355b80108c3ae0c6e6aea7aa312d63a58f51e65
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-03-02 21:15:37 +0530

    Adding comments

--- BatteryManagementConfig.h
+++ BatteryManagementConfig.h
@@ -1,18 +1,19 @@
 typedef struct
 {
-	float min_range;
-	float max_range;
-	char Min_Alertmessage[50];
-	char Max_Alertmessage[50];
+	float min_range;		// Lower Limit for BatteryHealth check
+	float max_range;		// Higher Limit for BatteryHealth check
+	char Min_Alertmessage[50];	// Alert message when orginal value is lower than min_range value
+	char Max_Alertmessage[50];	// Alert message when orginal value is higher than max_range value
 }BatteryManageConfig;
 
 
 BatteryManageConfig temperature_config={0,45,"Temperature is very low!","Temperature is very High!"};
 BatteryManageConfig soc_config={20,80,"State of Charge is very low!","State of Charge is very High!"};
 BatteryManageConfig chargeRate_config={0,0.8,"Charge Rate is very low!","Charge Rate is very low!"};
+BatteryManageConfig discharge_volt_config={3,4.2,"Discharge volt is very low!","Discharge volt is very low!"};
 
-
-enum BatteryHealth{
-	HEALTH_NOT_OK,
-	HEALTH_OK
+enum BatteryHealth
+{
+HEALTH_NOT_OK,
+EALTH_OK
 };
